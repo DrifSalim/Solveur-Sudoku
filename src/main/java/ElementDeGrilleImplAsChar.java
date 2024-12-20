b865588caa204ee42453640d6fe325a19e12eed7
metchime sara <sara.metchime@etud.u-picardie.fr>
2024-12-20 11:49:10 +0000

    methode getValue vide

--- src/main/java/ElementDeGrilleImplAsChar.java
+++ src/main/java/ElementDeGrilleImplAsChar.java
@@ -1,4 +1,8 @@
 import emiage.c306.sudoku.ElementDeGrille;
+import emiage.c306.sudoku.*;
 public class ElementDeGrilleImplAsChar implements ElementDeGrille{
     
+    public ElementDeGrille getValue(int x, int y) throws HorsBornesException {
+        return null;
+    }
 }
