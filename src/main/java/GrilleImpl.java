cf40f264b97c61698948beeed93e3941c010ad14
Drif Salim <salim.drif@etud.u-picardie.com>
2025-01-14 13:29:24 +0000

    Modification de GrilleImpl

--- src/main/java/GrilleImpl.java
+++ src/main/java/GrilleImpl.java
@@ -4,11 +4,23 @@ public class GrilleImpl implements Grille{
     public int getDimension(){
         return 0;
     }
-    public boolean isPossible(int x, int y) throws HorsBornesException, ElementInterditException {
+    public boolean isPossible(int x, int y, ElementDeGrille value)
+    {
         return false;
     }
     public ElementDeGrille getValue(int x, int y) throws HorsBornesException {
         return null;
     }
-    
+    public Set<ElementDeGrille> getElements(){
+        return null;
+    }
+    public boolean isComplete(){
+        return false;
+    }
+    public boolean isValeurInitiale(int x, int y){
+        return false;
+    }
+    public void setValue(int x, int y, ElementDeGrille value){
+
+    }
 }
