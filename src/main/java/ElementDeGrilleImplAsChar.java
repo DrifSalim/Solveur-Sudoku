9057c6c5054e259e3e492cbb59a73f25fe6442b0
Drif Salim <salim.drif@etud.u-picardie.com>
2025-01-14 13:43:01 +0000

    Modification du fichier ElementDeGrilleImpAsChar

---
+++ src/main/java/ElementDeGrilleImplAsChar.java
@@ -0,0 +1,13 @@
+import emiage.c306.sudoku.ElementDeGrille;
+
+public class ElementDeGrilleImplAsChar implements ElementDeGrille{
+    public boolean equals(Object obj){
+        return false;
+    }
+    public int hashCode(){
+        return 0;
+    }
+    public String toString(){
+        return "";
+    }
+}
