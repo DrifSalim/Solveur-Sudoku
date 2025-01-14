a92477287092ca730d629d1f5b82af0170bf4aac
Drif Salim <salim.drif@etud.u-picardie.com>
2025-01-14 13:59:22 +0000

    Ajout des methodes de tests

--- src/test/java/GrilleTest.java
+++ src/test/java/GrilleTest.java
@@ -1,7 +1,46 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
 
+import emiage.c306.sudoku.*;
+
 public class GrilleTest {
+    @Test
+    public void testDimensionGrille(){
+        int [][] grille = {
+            {1,1,1},
+            {2,2,2},
+            {3,3,3}
+        };
+        
+        //assertEquals(3, GrilleImpl.getDimension());
+        
+        int [][] grille2 = {
+            {1,1,1,1,1,1},
+            {2,2,2,2,2,2},
+            {3,3,3,3,3,3},
+            {3,3,3,3,3,3},
+            {3,3,3,3,3,3},
+            {3,3,3,3,3,3},
+        };
+        //assertEquals(6, );
+    }
+    @Test
+    public void testGetValue(){
+
+    }
+    @Test
+    public void testSetAndGetValue(){
+
+    }
+    @Test
+    public void testIsPossible(){
+
+    }
+    @Test
+    public void testIsComplete(){
+
+    }
     
 }
